9d910c1d11a4c287b7404b26065ccfcb3d6be28c
Darshan Satya <darshans1989@gmail.com>
2021-04-20 20:56:32 -0400

    Initial Setup

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,19 @@
+import styles from "./Header.module.css";
+
+imp
+
+const Header = (props) => {
+  return (
+    <>
+      <header>
+        <h1>Order Foooooood</h1>
+        <button>Cart</button>
+      </header>
+      <div>
+        <img />
+      </div>
+    </>
+  );
+};
+
+export default Header;
