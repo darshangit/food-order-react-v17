bb0493a5275903c0cf74a2b8a4af7699a42227e7
Darshan Satya <darshans1989@gmail.com>
2021-04-22 20:45:09 -0400

    Portal + Modal - Important

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,3 +1,4 @@
+import Modal from "../UI/Modal";
 import styles from "./Cart.module.css";
 
 const Cart = (props) => {
@@ -9,7 +10,7 @@ const Cart = (props) => {
     </ul>
   );
   return (
-    <div>
+    <Modal>
       {cartItems}
       <div className={styles.total}>
         <span>Total Amount</span>
@@ -19,7 +20,7 @@ const Cart = (props) => {
         <button className={styles["button--alt"]}>Close</button>
         <button className={styles.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   );
 };
 
