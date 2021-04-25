2acf722cb7f046e6fc4bb6bc92a01f334ce71f36
Darshan Satya <darshans1989@gmail.com>
2021-04-24 20:43:24 -0400

    Modal close and state management

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -10,14 +10,14 @@ const Cart = (props) => {
     </ul>
   );
   return (
-    <Modal>
+    <Modal onClose={props.onClose}>
       {cartItems}
       <div className={styles.total}>
         <span>Total Amount</span>
         <span>4922</span>
       </div>
       <div className={styles.actions}>
-        <button className={styles["button--alt"]}>Close</button>
+        <button className={styles["button--alt"]} onClick={props.onClose}>Close</button>
         <button className={styles.button}>Order</button>
       </div>
     </Modal>
